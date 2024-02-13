57b0d9fcd5cdf1f1b74abc0543f428b89bbb984d
YungBandulf <98176027+YungBandulf@users.noreply.github.com>
2024-02-13 20:51:04 +0100

    Update config.php

--- model/config.php
+++ model/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'putaindemotdepasse');
+define('DB_PASSWORD', 'xdddd');
 define('DB_NAME', 'compta_propre');
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
